fb88937373ea639bc42870b42cfe76946bd7cd2f
KryterX <49222266+KryterX@users.noreply.github.com>
2022-10-06 12:46:22 -0600

    Update

--- week3/Jumper/Game.py
+++ week3/Jumper/Game.py
@@ -2,7 +2,7 @@ import random
 import Director
 
 def SelectWord():
-    with open("E:\Programming\Python\CSE-210-1\week3\Hang Man\WordSelect.txt","r") as f:
+    with open("CSE-210-1\week3\Jumper\WordSelect.txt","r") as f:
         Lines=f.read().splitlines()
         random_line=random.choice(Lines)
         return random_line
