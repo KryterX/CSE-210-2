9cc8adf4a2cf991252dcddf323d9e4a67f11a084
KryterX <49222266+KryterX@users.noreply.github.com>
2022-10-15 21:19:05 -0600

    Update

---
+++ cycles/game/scripting/move_actors_action.py
@@ -0,0 +1,17 @@
+from game.scripting.action import Action
+
+
+class MoveActorsAction(Action):
+    """
+    An update action that moves all the actors.
+    
+    The responsibility of MoveActorsAction is to move all the actors that have a velocity greater
+    than zero.
+    """
+
+    def execute(self, cast, is_game_over, script):
+        """Executes the move actors action.
+        """
+        actors = cast.get_all_actors()
+        for actor in actors:
+            actor.move_next()
